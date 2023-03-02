201d5035b02cd00fc4047f5d19ee6fdd82e3b653
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-03-02 23:59:23 +0530

    Update Alternating Characters.js

--- Array and string/Alternating Characters.js
+++ Array and string/Alternating Characters.js
@@ -43,9 +43,6 @@
 
 // The characters marked red are the ones that can be deleted so that the string does not have matching adjacent characters.
 
-
-
-
 // *****************************************************************************************************************************************************************
 
 function alternatingCharacters(s) {
