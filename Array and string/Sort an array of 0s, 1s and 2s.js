7c07f5bcce6797df8f2a5df33a9b4b7ef2163d94
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-03-08 23:55:33 +0530

    Update Sort an array of 0s, 1s and 2s.js

--- Array and string/Sort an array of 0s, 1s and 2s.js
+++ Array and string/Sort an array of 0s, 1s and 2s.js
@@ -33,3 +33,23 @@
 // Constraints:
 // 1 <= N <= 10^6
 // 0 <= A[i] <= 2
+
+sort012(a, n){
+        let low = 0, high = n-1, mid = 0;
+        while(mid <= high){
+            if(a[mid]==0) {
+                let temp=a[low];
+                a[low]=a[mid];
+                a[mid]=temp;
+                low++;
+                mid++;
+            } else if(a[mid]==1) {
+                mid++;
+            } else {
+                let temp=a[mid];
+                a[mid]=a[high];
+                a[high]=temp;
+                high--;
+            }
+        }
+    }
