08369a5729e014e8e6efa29433c7f43344ed13ef
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-07 23:48:48 +0530

    Update Bill Division.js

--- Array and string/Bill Division.js
+++ Array and string/Bill Division.js
@@ -4,6 +4,7 @@
 
 // Function Description
 
+
 // Complete the bonAppetit function in the editor below. It should print Bon Appetit if the bill is fairly split. Otherwise, it should print the integer amount of money that Brian owes Anna.
 
 // bonAppetit has the following parameter(s):
