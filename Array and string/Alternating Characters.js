f76ed72bce8cf8d4249b3c3287bfd30dcc70dd9c
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-14 23:53:23 +0530

    Update Alternating Characters.js

--- Array and string/Alternating Characters.js
+++ Array and string/Alternating Characters.js
@@ -1,5 +1,4 @@
 // You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
-
 // Your task is to find the minimum number of required deletions.
 // Example
 // Remove an  at positions  and  to make  in  deletions.
