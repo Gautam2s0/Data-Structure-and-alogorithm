f48d35703663fdfb7f3a522a6404f8bbdc81a67c
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-19 09:39:05 +0530

    Update Masai_Airline.js

--- RM/Masai_Airline.js
+++ RM/Masai_Airline.js
@@ -1,1 +1,17 @@
 
+
+
+
+
+// *************************************************************************************
+const MasaiAirline=(n,arr1,arr2)=>{
+    for(let i=0;i<n;i++){
+        if(arr1[i]<=15&&arr2[i]<=7){
+            console.log("Boarding")
+        }else{
+            console.log("Stop")
+        }
+    }
+    
+    
+}
