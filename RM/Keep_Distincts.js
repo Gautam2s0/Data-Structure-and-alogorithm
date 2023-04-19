5cd06af4c206be057a4d4244e8530923cddfa013
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-19 09:37:12 +0530

    Update Keep_Distincts.js

--- RM/Keep_Distincts.js
+++ RM/Keep_Distincts.js
@@ -1,1 +1,32 @@
 
+// Description
+
+// Given a string S. Your task is to remove all duplicates characters from the string S
+
+// NOTE:
+
+// 1.) Order of characters in output string should be same as given in input string.
+
+// 2.) String S contains only lowercase characters ['a'-'z'].
+
+
+// Input
+// Input Format:
+
+// Input contain a single string S.
+
+// Constraints:
+
+// 1<=|Length of String|<=100000
+
+
+// Output
+// Print the string S with no any duplicate characters.
+
+
+// Sample Input 1 
+
+// iloveprogramming
+// Sample Output 1
+
+// iloveprgamn
