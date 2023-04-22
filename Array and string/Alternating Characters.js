4e723a13fc6ab65b8de7a0439daa11196fc73f66
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-22 23:48:03 +0530

    Update Alternating Characters.js

--- Array and string/Alternating Characters.js
+++ Array and string/Alternating Characters.js
@@ -4,7 +4,6 @@
 // Remove an  at positions  and  to make  in  deletions.
 // Function Description
 
-
 // Complete the alternatingCharacters function in the editor below.
 
 // alternatingCharacters has the following parameter(s):
