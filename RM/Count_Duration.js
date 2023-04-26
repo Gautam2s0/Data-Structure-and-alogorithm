23cc0c0b6ff2d77b7e6bbef1a0a6e7729de54ca8
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-26 23:35:07 +0530

    Update Count_Duration.js

--- RM/Count_Duration.js
+++ RM/Count_Duration.js
@@ -7,7 +7,6 @@
 
 // First line will be given by N test cases
 
-
 // Next N line will be given SH,SM,EH and EM  each separated by space(SH=starting hr,SM=starting min, EH=ending hr, EM=ending min)
 
 // Constraints
