09b9f8fe9aa6791b681afbdddfa7064da7e986a4
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-04-26 23:33:35 +0530

    Update Count_Duration.js

--- RM/Count_Duration.js
+++ RM/Count_Duration.js
@@ -2,7 +2,6 @@
 
 // Given 4 variables SH,SM,EH and EM  each separated by space(SH=starting hr, SM=starting min, EH=ending hr, EM=ending min), calculate and print the difference between the two times
 
-
 // Input
 // Input Format
 
