1b274bb047a45043df0c546fdb8c07cc348866fa
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-05-06 23:29:52 +0530

    Update Arrange_cars.js

--- RM/Arrange_cars.js
+++ RM/Arrange_cars.js
@@ -1,6 +1,5 @@
 // Description
  
-
 // For sure, the love mobiles will roll again on this summer's street parade. Each year, the organisers decide on a fixed order for the decorated trucks. Experience taught them to keep free a side street to be able to bring the trucks into order.
 
 // The side street is so narrow that no two cars can pass each other. Thus, the love mobile that enters the side street last must necessarily leave the side street first. Because the trucks and the ravers move up closely, a truck cannot drive back and re-enter the side street or the approach street.
