63186f47a52038491ea4afb7964d82aa6e380249
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-05-13 23:15:20 +0530

    Update Bill Division.js

--- Array and string/Bill Division.js
+++ Array and string/Bill Division.js
@@ -16,7 +16,7 @@
 // The first line contains two space-separated integers  and , the number of items ordered and the -based index of the item that Anna did not eat.
 // The second line contains  space-separated integers  where .
 // The third line contains an integer, , the amount of money that Brian charged Anna for her share of the bill.
-
+ 
 // Constraints
 
 // The amount of money due Anna will always be an integer
