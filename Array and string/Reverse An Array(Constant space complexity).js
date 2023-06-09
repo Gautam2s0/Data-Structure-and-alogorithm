2d8357e69dad8508c2b5cb81feba2f943a3a9eb6
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-09 22:54:27 +0530

    Update Reverse An Array(Constant space complexity).js

--- Array and string/Reverse An Array(Constant space complexity).js
+++ Array and string/Reverse An Array(Constant space complexity).js
@@ -4,3 +4,15 @@ Description
 Given an array of integers of length N, write a program to reverse the array with constant space complexity.
 
 Note: The approach should be a constant space complexity i.e, O(1)
+
+Input Description
+
+Input Format
+
+First line contains the length of the array N.
+
+Second line contains N number of integers separated by spaces.
+
+Constraints
+
+1 <= N<= 100
