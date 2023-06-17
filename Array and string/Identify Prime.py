0ae90a1f6f2b76dd442c1df4a00012818792220c
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-17 19:45:08 +0530

    Update Identify Prime.py

--- Array and string/Identify Prime.py
+++ Array and string/Identify Prime.py
@@ -7,3 +7,10 @@
 # If the value stored innum, is a prime number printYes, else printNo
 
 # Note : A prime number is a number, that is divisible by only 1 and the number itself
+
+Input
+The first and the only line of the input contains the value stored innum
+
+
+Output
+If the value stored innum, is a prime number printYes, else printNo
