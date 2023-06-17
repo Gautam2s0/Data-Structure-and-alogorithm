4289d7684bfadf62c0812818dfa4a2a6babf669a
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-17 19:46:21 +0530

    Update Identify Prime.py

--- Array and string/Identify Prime.py
+++ Array and string/Identify Prime.py
@@ -1,23 +1,23 @@
-# # Description
+# # # Description
 
-# # You are given a number stored in a variable with the namenum
+# # # You are given a number stored in a variable with the namenum
 
-# # Check if the number is a prime number or not
+# # # Check if the number is a prime number or not
 
+# # # If the value stored innum, is a prime number printYes, else printNo
+
+# # # Note : A prime number is a number, that is divisible by only 1 and the number itself
+
+# # Input
+# # The first and the only line of the input contains the value stored innum
+
+
+# # Output
 # # If the value stored innum, is a prime number printYes, else printNo
 
-# # Note : A prime number is a number, that is divisible by only 1 and the number itself
+# Sample Input 1 
 
-# Input
-# The first and the only line of the input contains the value stored innum
+# 13
+# Sample Output 1
 
-
-# Output
-# If the value stored innum, is a prime number printYes, else printNo
-
-Sample Input 1 
-
-13
-Sample Output 1
-
-Yes 
+# Yes 
