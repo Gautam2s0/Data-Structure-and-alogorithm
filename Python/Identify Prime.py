b998265306c9fc0bc6b69c33bf92cdc1cffcef2a
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-22 23:00:55 +0530

    Update Identify Prime.py

--- Python/Identify Prime.py
+++ Python/Identify Prime.py
@@ -5,7 +5,7 @@
 # Check if the number is a prime number or not 
 
 # If the value stored innum, is a prime number printYes, else printNo
-
+ 
 # Note : A prime number is a number, that is divisible by only 1 and the number itself
 
 # Input
