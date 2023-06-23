feaf591b3a8d8647350463cb865cb090d1db3e75
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-23 10:52:48 +0530

    Update Remove_duplicates.js

--- JavaScript/Array and string/Remove_duplicates.js
+++ JavaScript/Array and string/Remove_duplicates.js
@@ -1,1 +1,17 @@
-
+const Rmdup=(n,arr)=>{
+  let res={}
+  for(let el of arr){
+    if(res[el]){
+      continue
+    }
+    else{
+      res[el]=1
+    }
+  }
+  let p=[]
+  for(let key in res){
+   p.push(key)
+  }
+  console.log(p.length)
+  console.log(p.join(' '))
+}
