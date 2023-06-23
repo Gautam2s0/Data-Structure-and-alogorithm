a16fa1edcf003ab0fd0bc222f07361cc6b3893ed
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-23 10:36:56 +0530

    Swap_to_Maximize

--- JavaScript/Array and string/Swap_to_maximize.js
+++ JavaScript/Array and string/Swap_to_maximize.js
@@ -3,3 +3,32 @@
 // Nick has a habit of collecting stamps. He has n number of stamps with him and each stamp has a certain value (x0,x1,x2,....).Nick's friend gave him an offer that since he also has stamps of values (y0,y1,y2...) Nick can exchange or swap any of his stamps with Neel's stamps but the exchange/swaps are at most k times.
 
 // Your task is to find the maximum possible sum of stamp values Nick can obtain in the array X (Nick's array of stamps) if he can do no more than (i.e. at most) K such exchanges or swaps.
+
+// Input Description
+
+// Input Format
+
+// The first line of the input contains one integer t — the number of test cases. Then test cases follow.
+
+// The first line of the test case contains two integers n and k — the number of elements in X and Y and the maximum number of moves he can do.
+
+// The second line of the test case contains n integers x0,x1,x2,…,x(n-1)
+
+// The third line of the test case contains n integers y0,y1,y2,…,y(n-1)
+
+// Constraints
+
+// 1 ≤t≤200
+
+// 1 ≤ n ≤ 30
+
+// 0 ≤ k ≤ n
+
+// 1 ≤ xi ≤ 30
+
+// 1 ≤ yi ≤ 30
+
+
+// Output Description
+
+// For each test case, print the answer — the maximum possiblesum he can obtain in the array Xhe can do no more than kswaps.
