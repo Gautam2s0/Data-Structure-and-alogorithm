4993089f1ff196d8cc2b5b4c07a14bf45ebc2691
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-23 10:49:15 +0530

    Update Rotate_the_array.js

--- JavaScript/Array and string/Rotate_the_array.js
+++ JavaScript/Array and string/Rotate_the_array.js
@@ -1,44 +1,44 @@
-Description
+// Description
 
-You are given an unsorted array of n elements and an integer k, you need to rotate the array by k units towards right direction ( => ).
+// You are given an unsorted array of n elements and an integer k, you need to rotate the array by k units towards right direction ( => ).
 
 
-Input Description
+// Input Description
 
-In the first line, we have $T$, the number of testcases.
-For Every testcase, We have Two Lines of Input.
-1. We have $N$ and $K$.
-2. $N$ Integers of array separated by space.
+// In the first line, we have $T$, the number of testcases.
+// For Every testcase, We have Two Lines of Input.
+// 1. We have $N$ and $K$.
+// 2. $N$ Integers of array separated by space.
 
-Constraints
+// Constraints
 
-1<=T<=10
-1<=N<=1000
-0<=K<=1000
-1 <= A[i] <= 1000000
+// 1<=T<=10
+// 1<=N<=1000
+// 0<=K<=1000
+// 1 <= A[i] <= 1000000
 
-Output Description
+// Output Description
 
-output N elements, elements of the array , rotated by K units
+// output N elements, elements of the array , rotated by K units
 
-Sample Input 1
+// Sample Input 1
 
-Copy
+// Copy
 
-3
-3 1
-1 2 3
-2 2
-1 2
-2 3
-1 2
-Sample Output 1
+// 3
+// 3 1
+// 1 2 3
+// 2 2
+// 1 2
+// 2 3
+// 1 2
+// Sample Output 1
 
-Copy
+// Copy
 
-3 1 2
-1 2
-2 1
+// 3 1 2
+// 1 2
+// 2 1
 
 
 const rotate=(n,k,arr)=>{
