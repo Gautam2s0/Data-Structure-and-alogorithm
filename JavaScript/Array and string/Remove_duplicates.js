121c9bd359b5c0d4fa6656dbf5bc48c6070135c8
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-06-23 10:54:04 +0530

    Update Remove_duplicates.js

--- JavaScript/Array and string/Remove_duplicates.js
+++ JavaScript/Array and string/Remove_duplicates.js
@@ -1,34 +1,34 @@
-Description
+// Description
 
-Given an array of n integers, the array is sorted. You have to remove the duplicates, print only unique elements, do it in place. i.e O(1) space
-
-
-Input Description
-
-1<=T<=10
-
-1<=N<=100000
-
-1<=Ai<=100000
+// Given an array of n integers, the array is sorted. You have to remove the duplicates, print only unique elements, do it in place. i.e O(1) space
 
 
-Output Description
+// Input Description
 
-output a single integer x, i.e the number of unique elements in the array and in the next line print the x unique elements.
+// 1<=T<=10
 
-  Sample Input 1
+// 1<=N<=100000
 
-2
-3
-1 1 2
-4
-1 1 3 3
-Sample Output 1
+// 1<=Ai<=100000
 
-2
-1 2
-2
-1 3
+
+// Output Description
+
+// output a single integer x, i.e the number of unique elements in the array and in the next line print the x unique elements.
+
+//   Sample Input 1
+
+// 2
+// 3
+// 1 1 2
+// 4
+// 1 1 3 3
+// Sample Output 1
+
+// 2
+// 1 2
+// 2
+// 1 3
 
 
 
