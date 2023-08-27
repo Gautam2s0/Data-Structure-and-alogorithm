0db053d3780ec8b77f3aa095d2f5c7e4d5f8a50e
Gautam Sonkar <106190856+Gautam2s0@users.noreply.github.com>
2023-08-27 23:04:23 +0530

    Update MerseSortedArray.py

--- Python/MerseSortedArray.py
+++ Python/MerseSortedArray.py
@@ -34,7 +34,7 @@
 
 # #######################################################################################################################
 
-
+# defining the function 
 def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
