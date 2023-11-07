1df9f615cd68c7c8e31f1ded0cd39d1745089feb
Gautam2S0 <sonkarg117@gmail.com>
2023-11-07 16:47:25 +0530

    swap2num

--- C/printing_Value_Out_Of_Range.c
+++ C/printing_Value_Out_Of_Range.c
@@ -4,10 +4,13 @@ int main()
 {
    int a=32768;
   int b=65536;
+  int c=-10;
   printf("%hd\n",a);
   // the value of signed int is not in range of -32768 to 32767 so it will print start value that is -32768
   printf("%hu",b);
   // the value of unsigned int is not in range of 0 to 65535 so it will print start value that is 0
+ printf("%hu",a);
+  // the value of c unsigned int is not in range of 0 to 65535 but it try to find in reverse direction(like -1,-2,..) it will find 65535 so it will print 65535 
 
     return 0;
 }
