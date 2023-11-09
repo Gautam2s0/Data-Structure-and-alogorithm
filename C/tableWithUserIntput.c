a623ad123995a2899a5db9e5a57a5c66c56a1c5f
Gautam2S0 <sonkarg117@gmail.com>
2023-11-09 10:00:01 +0530

    table

---
+++ C/tableWithUserIntput.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int i,j;
+    printf("Enter a number: ");
+    scanf("%d",&j);
+    for(i=1;i<=10;i++){
+       
+            printf("%d\n ",i*j);
+        }
+         printf("\n");
+       
+  
+
+    return 0;
+}
