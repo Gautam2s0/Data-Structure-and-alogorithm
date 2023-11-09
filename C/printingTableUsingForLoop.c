8c32e0ecddb08e28ecfd11f102725482cda97dae
Gautam2S0 <sonkarg117@gmail.com>
2023-11-09 09:56:18 +0530

    table

--- C/printingTableUsingForLoop.c
+++ C/printingTableUsingForLoop.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-    int i,j;
-    for(i=1;i<=10;i++){
-        for(j=1;j<=10;j++){
-            // if(i==j){
-            printf("%d ",i*j);
+    int i, j;
+    for (i = 1; i <= 10; i++)
+    {
+        for (j = 1; j <= 10; j++)
+        {
+
+            printf("%d ", i * j);
         }
-         printf("\n");
-        // }
+        printf("\n");
     }
 
     return 0;
