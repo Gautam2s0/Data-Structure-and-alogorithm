b2f53ec84de24c62d936d3eee7e1072fe3ee9b0e
Gautam2S0 <sonkarg117@gmail.com>
2023-11-14 11:56:52 +0530

    NestedLoop

--- C/nestedLoop.c
+++ C/nestedLoop.c
@@ -1,14 +1,22 @@
+//  odd Even Sum using simple loop
+//***************************************************************88
 #include <stdio.h>
 int main(){
-  int i,sum=0;
-  for(i=1;i<=5;i++){
-    if(i==2){
-      continue;
+  int i,j,sumOdd=0,SumEv=0;
+  printf("Enter a number");
+  scanf("%d",&j);
+  for(i=1;i<=j;i++){
+    if(i%2==0){
+        SumEv+=i;
+    //   continue;
     }
     else{
-      sum+=i;
+      sumOdd+=i;
     }
   }
-  printf("sum = %d",sum);
+  printf("Sum Of Even Number = %d\n",SumEv);
+  printf("Sum Of Odd Number = %d\n",sumOdd);
   return 0;
 }
+
+//***********************************************************************************8
