ae3d4a44189ad8ee3c006abceb4e6c9e3ded31a6
Gautam2S0 <sonkarg117@gmail.com>
2023-11-14 12:41:50 +0530

    HollowStartPattern

--- C/nestedLoop.c
+++ C/nestedLoop.c
@@ -1,23 +1,23 @@
 //  odd Even Sum using simple loop
 //***************************************************************88
-// #include <stdio.h>
-// int main(){
-//   int i,j,sumOdd=0,SumEv=0;
-//   printf("Enter a number");
-//   scanf("%d",&j);
-//   for(i=1;i<=j;i++){
-//     if(i%2==0){
-//         SumEv+=i;
-//     //   continue;
-//     }
-//     else{
-//       sumOdd+=i;
-//     }
-//   }
-//   printf("Sum Of Even Number = %d\n",SumEv);
-//   printf("Sum Of Odd Number = %d\n",sumOdd);
-//   return 0;
-// }
+#include <stdio.h>
+int main(){
+  int i,j,sumOdd=0,SumEv=0;
+  printf("Enter a number");
+  scanf("%d",&j);
+  for(i=1;i<=j;i++){
+    if(i%2==0){
+        SumEv+=i;
+    //   continue;
+    }
+    else{
+      sumOdd+=i;
+    }
+  }
+  printf("Sum Of Even Number = %d\n",SumEv);
+  printf("Sum Of Odd Number = %d\n",sumOdd);
+  return 0;
+}
 
 //***********************************************************************************//
 // star hollow pattern printing using for loop taking user input
@@ -52,6 +52,66 @@ int main()
                 {
                     printf("  ");
                 }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+//    ****************************************** Right Triangle ******************************
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * * * *
+
+#include <stdio.h>
+int main()
+{
+    int i, j, l;
+    printf("Enter first number for pattern : ");
+    scanf("%d", &j);
+    for (i = 0; i < j; i++)
+    {
+        for (l = 0; l < i + 1; l++)
+        {
+
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+//    ****************************************** Left Right angle Triangle ******************************
+
+//           *
+//         * *
+//       * * *
+//     * * * *
+//   * * * * *
+// * * * * * *
+
+#include <stdio.h>
+int main()
+{
+    int i, j, l;
+    printf("Enter first number for pattern : ");
+    scanf("%d", &j);
+    for (i = 0; i < j; i++)
+    {
+        for (l = 0; l < j; l++)
+        {
+            if (l < j - i-1)
+            {
+                printf("  ");
+            }
+            else
+            {
+                printf("* ");
             }
         }
         printf("\n");
