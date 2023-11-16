61d98b26af4aee810ec418a5d466f8f275622017
Gautam2S0 <sonkarg117@gmail.com>
2023-11-16 14:46:37 +0530

    Array

--- C/ReverString.c
+++ C/ReverString.c
@@ -2,5 +2,15 @@
 #include<string.h>
 
 void main(){
-    
+    char string [30]="Gautam";
+    int i=0,l;
+    char c;
+    l=strlen(string);
+    for(i;i<l/2;i++){
+        c=string[i];
+        string[i]=string[l-1-i];
+        string[l-i-1]=c;
+    }
+    printf("%s",string);
+
 }
