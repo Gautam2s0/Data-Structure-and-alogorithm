1c420a390e1d137db493f74ea1c429aaeebf2918
Gautam2S0 <sonkarg117@gmail.com>
2023-11-16 18:29:01 +0530

    Pointer

--- C/ReverseArrayUsingPointer.c
+++ C/ReverseArrayUsingPointer.c
@@ -1,5 +1,12 @@
 // Write a program in C to print the elements of an array in reverse order.
 #include <stdio.h>
+#define MAX 5 
 void main(){
-    
+    int arr[MAX];
+    int i;
+    printf("Enter element of Array : ");
+    for(i=0;i<MAX;i++){
+        
+    }
+
 }
