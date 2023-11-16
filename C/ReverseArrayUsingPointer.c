4575bc4d61871bbf62540285c8c7fafd1ddf7f34
Gautam2S0 <sonkarg117@gmail.com>
2023-11-16 18:27:29 +0530

    Pointer

---
+++ C/ReverseArrayUsingPointer.c
@@ -0,0 +1,5 @@
+// Write a program in C to print the elements of an array in reverse order.
+#include <stdio.h>
+void main(){
+    
+}
--- C/SumOfAllELEMentusingPointer.c
+++ C/SumOfAllELEMentusingPointer.c
@@ -6,16 +6,14 @@ void main(){
     int arr[MAX];
     int *el,i,l;
     int sum=0;
+    el=&arr[0];
     printf("Enter the array element ");
     for(i=0;i<MAX;i++){
         scanf("%d",&arr[i]);
     }
-    el=arr;
-
-    l=sizeof(arr)/sizeof(arr[0]);
-    for(i=0;i<l;i++){
+    
+    for(i=0;i<MAX;i++){
       sum+=*el;
-      printf("sum of array at index %d is %d\n",i,sum);
       el++;
     }
     printf("Sum Of array is %d ",sum);
