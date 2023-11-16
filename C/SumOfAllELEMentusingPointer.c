3f2d2359414d2227ed7086b6353fd1df0c020e5c
Gautam2S0 <sonkarg117@gmail.com>
2023-11-16 17:52:34 +0530

    Pointer

--- C/SumOfAllELEMentusingPointer.c
+++ C/SumOfAllELEMentusingPointer.c
@@ -3,7 +3,22 @@
 #include <stdio.h>
 #define MAX 100
 void main(){
-    
+    int arr[MAX];
+    int *ch,i;
+    int sum=0;
+    printf("Enter the array element ");
+    for(i=0;i<MAX;i++){
+        scanf("%d",&arr[i]);
+    }
+
+
+// *ch =value or address of every character in string  
+    while(*ch!='\0'){
+        l++;
+        ch++;
+
+    }
+    printf("Length Of String is %d ",l);
     
 
 }
