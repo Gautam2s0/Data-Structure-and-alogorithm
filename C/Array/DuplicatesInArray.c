5554a4461e36c07a62e0ef4f91809bdfbfbd4de1
Gautam2S0 <sonkarg117@gmail.com>
2023-11-17 15:03:06 +0530

    Pointer

--- C/Array/DuplicatesInArray.c
+++ C/Array/DuplicatesInArray.c
@@ -1,8 +1,16 @@
 // Given an array of n elements that contains elements from 0 to n-1, 
 // with any of these numbers appearing any number of times. Find these repeating numbers 
 
+// Note: The repeating element should be printed only once.
+
+// Example: 
+
+// Input: n=7 , array[]={1, 2, 3, 6, 3, 6, 1}
+// Output: 1, 3, 6
+// Explanation: The numbers 1 , 3 and 6 appears more than once in the array.
+
 #include <stdio.h>
 int main(){
-    
+
     return 0;
 }
