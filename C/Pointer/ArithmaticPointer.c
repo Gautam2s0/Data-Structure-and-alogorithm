7ce233ceaf6c90a2edc54cbaca5a1ce49b3594b6
Gautam2S0 <sonkarg117@gmail.com>
2023-11-17 11:45:15 +0530

    Pointer

---
+++ C/Pointer/ArithmaticPointer.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void  main(){
+    int a[]={9,5,6,0,4,44,33,70,96};
+    int *p=&a;
+    int *q=&a[7];
+    int x;
+    // ******************************    Addition **************************************************888888
+    //x=p+q; //error we can't add two pointer we can add integer in pointer
+    x=*(p+3);
+    printf("%d\n",*(p+2));              
+    printf("%d\n",x);
+    // printf("%d",*(p+q));  
+    // error invalid operands to binary + (have 'int *' and 'int *')
+    // printf("%d",p+q);
+
+
+   // *************************************** Substraction  **********************************
+ 
+//    printf( "substraction %d\n",*(q-2)); // gives address 
+//    printf( "substraction %d\n",(q-p)); //give the interger value (number of element)
+
+   // incerement and decrement operator
+    p++;
+    printf("Increment %d \n",*p);
+    printf("Increment %d \n",*p++);
+    printf("Increment %d \n",*++p);
+    printf("Decrement %d %d %d \n",*--q,*--q,*--q); // evaluate right to left
+    
+}
