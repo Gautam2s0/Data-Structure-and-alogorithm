8d27fa8b7bed02bbd5578ffc8a066aa375b3e6c7
Gautam2S0 <sonkarg117@gmail.com>
2023-11-17 15:02:34 +0530

    Pointer

---
+++ C/Array/DuplicatesInArray.c
@@ -0,0 +1,8 @@
+// Given an array of n elements that contains elements from 0 to n-1, 
+// with any of these numbers appearing any number of times. Find these repeating numbers 
+
+#include <stdio.h>
+int main(){
+    
+    return 0;
+}
