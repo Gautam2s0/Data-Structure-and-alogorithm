e1c6e593318b4acd5cc12a6a2f5a692f4a446ca1
Gautam2S0 <sonkarg117@gmail.com>
2023-11-17 14:32:12 +0530

    Pointer

---
+++ C/Array/given_Sum_exists_in_Array.c
@@ -0,0 +1,6 @@
+// Given an array A[] of n numbers and another number x, 
+// the task is to check whether or not there exist two elements in A[] whose sum is exactly x. 
+
+// Input: arr[] = {0, -1, 2, -3, 1}, x= -2
+// Output: Yes
+// Explanation:  If we calculate the sum of the output,1 + (-3) = -2
