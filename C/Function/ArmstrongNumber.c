cdb5e332bafb875a0e5257e4211a5cd40a1b7d79
Gautam2S0 <sonkarg117@gmail.com>
2023-11-21 19:25:58 +0530

    11/21/23

--- C/Function/ArmstrongNumber.c
+++ C/Function/ArmstrongNumber.c
@@ -6,3 +6,17 @@
 //  The 371 is an Armstrong number.                                                 
 //  The 371 is not a Perfect number.
 
+#include <stdio.h>
+#include <stdbool.h>
+bool IsArmstrong(int);
+
+void main(){
+    int num=371;
+    bool flag;
+    flag=IsArmstrong(num);
+    printf("Largest number is : %d",flag);
+}
+bool IsArmstrong(int num){
+    
+
+}
