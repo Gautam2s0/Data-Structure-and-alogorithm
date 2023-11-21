e86bf372edc78808dcdccdea32dd94b9dc2cc14b
Gautam2S0 <sonkarg117@gmail.com>
2023-11-21 16:15:29 +0530

    11/21/23

--- C/Function/ReturnStringFromFunction.c
+++ C/Function/ReturnStringFromFunction.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-char* display();
-void main(){
-    printf("String = %s",display());
+ char *display();
+void main()
+{
+     char *str;
+    str = display();
+    str[0] = 'M';
+    printf("String is %s", str);
 }
-// to return a string from function 
-char display(){
-    
-    return "Gautam";
-}
-// here display function return only the first character of Gautam that is G;
-// so return complete string use * like bellow
-// char * display(){
-//     return "Gautam"
+
+// to return a string from function
+// char  display(){
+//     return "Gautam";
 // }
 
+// here display function return only the first character of Gautam that is G or nothing will print;
+// so return complete string use * like bellow
+// char* display(){
+//     return "Gautam";
+// }
 
+// to modify the string .
+ char *display()
+{
+    static char str[60] ="Gautam";
+    return str;
+}
