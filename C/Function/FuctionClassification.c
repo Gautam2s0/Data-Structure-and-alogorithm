b6f1b06c020b84c12f0ea921b4542d22f78e5e93
Gautam2S0 <sonkarg117@gmail.com>
2023-11-21 11:14:48 +0530

    11/21/23

--- C/Function/FuctionClassification.c
+++ C/Function/FuctionClassification.c
@@ -4,4 +4,31 @@
 // 3. WITH ARGUMENT WITHOUT RETURN TYPE 
 // 4. WITH ARGUMENT WITH RETURN TYPE 
 
-// NO AGRUMEN WITHOUT RETURN TYPE 
+//  *******************    NO AGRUMEN WITHOUT RETURN TYPE  ********************** 
+
+
+//  #include <stdio.h>
+
+// void func(void);
+// void main(){
+//     func();
+// }
+// void func(){
+//     printf("Gautam Sonkar");
+// }
+
+// *******************  NO ARGUMENT WITH RETURN TYPE  *************************
+
+
+#include <stdio.h>
+
+
+void main(){
+    int p;
+    p=func();
+    printf("p=%d",p);
+}
+void func(){
+    int x=68;
+   return x;
+}
