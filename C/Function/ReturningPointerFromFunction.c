8ca4534d169c82f49052c4b43bb317937749ee6a
Gautam2S0 <sonkarg117@gmail.com>
2023-11-21 17:51:00 +0530

    11/21/23

---
+++ C/Function/ReturningPointerFromFunction.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int* returnPointer(int[]); // pointer type function
+
+void main(){
+    int arr[20]={1,2,3,4,5,7,9};
+    int *p;
+    // storing the pointer in p so p should be pointer variable
+    p=returnPointer(arr);
+    printf("p=%d",*p);
+}
+
+int* returnPointer(int a[]){
+    // function will return pointer not a value
+    a=a+2;
+    return a;
+}
