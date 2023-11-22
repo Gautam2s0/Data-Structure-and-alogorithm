96d63b8582242201a814fff7d5c6d0e0318ef103
Gautam2S0 <sonkarg117@gmail.com>
2023-11-22 16:19:36 +0530

    22/11/23

--- C/Function/Questions/Q6.c
+++ C/Function/Questions/Q6.c
@@ -89,8 +89,12 @@
 #include <stdio.h>
     int main()
     {
-        int i = 10;
+        int i = 18;
         void *p = &i;
-        printf("%f\n", *(float*)p);
+        printf("%d\n", (int*)p);  //6422296 address of i
+         printf("%d\n", *(int*)p);  //18 value of i
+        printf("%f\n", *(float*)p); // 0.000000 float value which is present on location 6422296 || picking float value  
+        printf("%f\n", (float)*(float*)p); // 0.000000 float value which is present on location 6422296 || picking float value
+        printf("%f\n", (float)*(int*)p); // 18.000000 float value which is present on integer location  6422296
         return 0;
     }
