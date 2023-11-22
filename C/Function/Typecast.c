3c1de73564d1392c641ea52b87478aef0a86b3f3
Gautam2S0 <sonkarg117@gmail.com>
2023-11-22 16:37:05 +0530

    22/11/23

--- C/Function/Typecast.c
+++ C/Function/Typecast.c
@@ -3,15 +3,25 @@
             //  new_datatype *new_pointerName=(new_dataype *)original_pointer
 
 
+// #include <stdio.h>
+//     int main()
+//     {
+//         int i = 18;
+//         void *p = &i;
+//         printf("%d\n", (int*)p);  //6422296 address of i
+//          printf("%d\n", *(int*)p);  //18 value of i
+//         printf("%f\n", *(float*)p); // 0.000000 float value which is present on location 6422296 || picking float value  
+//         printf("%f\n", (float)*(float*)p); // 0.000000 float value which is present on location 6422296 || picking float value
+//         printf("%f\n", (float)*(int*)p); // 18.000000 float value which is present on integer location  6422296
+//         return 0;
+//     }
+
 #include <stdio.h>
-    int main()
+    void main()
     {
-        int i = 18;
-        void *p = &i;
-        printf("%d\n", (int*)p);  //6422296 address of i
-         printf("%d\n", *(int*)p);  //18 value of i
-        printf("%f\n", *(float*)p); // 0.000000 float value which is present on location 6422296 || picking float value  
-        printf("%f\n", (float)*(float*)p); // 0.000000 float value which is present on location 6422296 || picking float value
-        printf("%f\n", (float)*(int*)p); // 18.000000 float value which is present on integer location  6422296
-        return 0;
+        int x = 0;
+        int *ptr = &x;
+        printf("%p\n", ptr);
+        ptr++;
+        printf("%p\n ", ptr);
     }
