5c910cc5a7cb2aee2bbc31dbe93e3973546a33d4
Gautam2S0 <sonkarg117@gmail.com>
2023-11-22 13:17:03 +0530

    22/11/23

--- C/Function/CallBackFunction/ApplicationOfFunctionPointer.c
+++ C/Function/CallBackFunction/ApplicationOfFunctionPointer.c
@@ -1,38 +1,39 @@
-#include <stdio.h>
-void sum(int a,int b){
-    printf("Sum = %d\n",a+b);
-}
-void sub(int a,int b){
-    printf("Sub = %d\n",a-b);
-}
-void mul(int a,int b){
-    printf("Mul = %d\n",a*b);
-}
-void div(int a,int b){
-    printf("Div = %d\n",a/b);
-}
+// #include <stdio.h>
+// void sum(int a,int b){
+//     printf("Sum = %d\n",a+b);
+// }
+// void sub(int a,int b){
+//     printf("Sub = %d\n",a-b);
+// }
+// void mul(int a,int b){
+//     printf("Mul = %d\n",a*b);
+// }
+// void div(int a,int b){
+//     printf("Div = %d\n",a/b);
+// }
 
-// declaring a funnction that take another function as parameter
-// here (*fptr) is a pointer function that takes two integer type parameter
-void display(void (*fptr)(int,int)){
-    (*fptr)(9,5);
-}
-void main(){
-    // 1st method
-    display(sum);
-    display(sub);
-    display(mul);
-    display(div);
-    // second method
+// // declaring a funnction that take another function as parameter
+// // here (*fptr) is a pointer function that takes two integer type parameter
+// void display(void (*fptr)(int,int)){
+//     (*fptr)(9,5);
+// }
+// void main(){
+//     // 1st method
+//     display(sum);
+//     display(sub);
+//     display(mul);
+//     display(div);
+//     // second method
 
-    int a,b,ch;
-    void (*fptr[10])(int,int)={sum,sub,mul,div};
-    printf("Enter Ch : ");
-    scanf("%d",&ch);
-    printf("Enter a and b : ");
-    scanf("%d %d",&a,&b);
-    (*fptr[ch])(a,b);
+//     int a,b,ch;
+//     void (*fptr[10])(int,int)={sum,sub,mul,div};
+//     printf("Enter Ch : ");
+//     scanf("%d",&ch);
+//     printf("Enter a and b : ");
+//     scanf("%d %d",&a,&b);
+//     (*fptr[ch])(a,b);
 
 
 
-}
+// }
+
