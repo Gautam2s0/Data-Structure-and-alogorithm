4aaeb742c7069f426fcfa3cf45b1ca36267aa463
Gautam2S0 <sonkarg117@gmail.com>
2023-11-23 18:29:57 +0530

    23/11/23

--- C/Structure/StructurePointer.c
+++ C/Structure/StructurePointer.c
@@ -1,1 +1,19 @@
 // Pointer of structure
+
+#include <stdio.h>
+
+struct StructurePointer
+{
+   int num;
+   char name[20];
+};
+
+struct StructurePointer s;
+
+
+void main(){
+    s.num=30;
+    s.name="Gautam";
+    struct StructurePointer *ptr=&s;
+    printf("Number = %d\nName = %s",ptr->num,ptr->name);
+}
