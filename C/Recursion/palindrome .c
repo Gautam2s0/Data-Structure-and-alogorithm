08a0faffc615b300e5a0a4ad9aee81247c64370e
Gautam2S0 <sonkarg117@gmail.com>
2023-11-23 15:21:23 +0530

    23/11/23

--- C/Recursion/palindrome .c
+++ C/Recursion/palindrome .c
@@ -1,18 +1,34 @@
 // Write a C program to check whether a given string is a palindrome or not using recursion.
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-void palindrome (int n){
-    static int d=1;
-    printf("%d\n",n);
-    printf("%d\n",d);
-    d++;
-    if(n>1) count(n-1);
-    printf("%d\n",d);
+bool palindrome(char str[],int len)
+{
+   static int i=0;
+    if(str[i]!=str[len-1]&&i<=len-1){
+        return false;
+    }
+    if(len-1==i||i>=len/2){
+        return true;
+    }
+    
+    i++;
+    len--;
+    palindrome(str,len);
 }
-
 
 
 void main(){
     // count(3);
-    printf("sum = %d",print(50));
+    bool res;
+    int len;
+    char str[60];
+    printf("Enter a string : ");
+    gets(str);
+
+    res=palindrome(str,strlen(str));
+    res?printf("Yes"):printf("False");
+    // printf("sum = %d",print(50));
+    
 }
