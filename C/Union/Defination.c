42c8930814802ea414270bdcd14602d88cbb76ce
Gautam2S0 <sonkarg117@gmail.com>
2023-11-24 11:01:28 +0530

    24/11/23

---
+++ C/Union/Defination.c
@@ -0,0 +1,37 @@
+// The Union is a user-defined data type in C language that can contain 
+// elements of the different data types just like structure. 
+// But unlike structures, all the members in the C union are stored in the same memory location. 
+// Due to this, only one member can store data at the given instance.
+
+
+// union union_name {
+//     datatype member1;
+//     datatype member2;
+//     ...
+// };
+
+// C Program to demonstrate how to use union
+#include <stdio.h>
+
+// union template or declaration
+union un {
+	int member1;
+	char member2;
+	float member3;
+};
+
+// driver code
+int main()
+{
+
+	// defining a union variable
+	union un var1;
+
+	// initializing the union member
+	var1.member1 = 15;
+
+	printf("The value stored in member1 = %d",
+		var1.member1);
+
+	return 0;
+}
