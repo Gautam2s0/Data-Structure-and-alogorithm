16e5f00a92c20b1666c17ea98bd4ff7644b3d946
Gautam2S0 <sonkarg117@gmail.com>
2023-11-27 17:01:41 +0530

    27/11/23

--- C/Structure/Q1.c
+++ C/Structure/Q1.c
@@ -7,7 +7,7 @@ int main()
 		int bits_2: 4;
 		int bits_3: 2;
 		int bits_4: 3;
-	}bit = {2, 3, 7, 7};
+	}bit = {2, 3, 8, 7};
 	printf("%d %d %d %d", bit.bits_1, bit.bits_2, bit.bits_3, bit.bits_4);
 }
 
