703497e15da4356b3aac6b57192759387684d003
Gautam2S0 <sonkarg117@gmail.com>
2023-11-27 12:47:13 +0530

    27/11/23

--- C/Pointer/Accessing2DArrayElement.c
+++ C/Pointer/Accessing2DArrayElement.c
@@ -13,3 +13,39 @@
 // *(*matrix + 1)       =>    Points to matrix[0][1]
 // *(*(matrix + 0) + 1) =>    Points to matrix[0][1]
 // *(*(matrix + 2) + 2) =>    Points to matrix[2][2]
+
+#include <stdio.h>
+#define row 3 
+#define col 4
+
+void main(){
+    int mat[row][col]={{1,2,3,0},{4,5,6,0},{7,8,9,0}};
+    int (*p)[col] = mat,i,j;
+    // s=sizeof(mat)/sizeof(mat[0]); row
+
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++){
+            // *(*(p + i) + j) is equivalent to p[i][j]
+           printf("%d ", *(*( p+ i) + j));
+        }
+        printf("s\n");
+    }
+
+    printMat(mat,row,col);
+}
+
+void printMat(int (*matrix)[col], int rows, int cols){
+     int i, j;
+
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            // *(*(matrix + i) + j) is equivalent to matrix[i][j]
+            printf("%d ", *(*(matrix + i) + j));
+        }
+
+        printf("\n");
+    }
+}
