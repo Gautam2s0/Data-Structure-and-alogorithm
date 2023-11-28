6e01c30beaed63190096c4b6ff68162c395948e2
Gautam2S0 <sonkarg117@gmail.com>
2023-11-28 09:50:00 +0530

    27/11/23

--- C/Pointer/ReverseString.c
+++ C/Pointer/ReverseString.c
@@ -4,7 +4,7 @@
 
 #include <stdio.h>
 #define MAX_SIZE 100 // Maximum string size
-void reverse(char *str[],char *rev[]);
+void reverse(char *str[], char *rev[]);
 
 int main()
 {
@@ -16,12 +16,12 @@ int main()
     /* Input string from user */
     printf("Enter any string: ");
     gets(str);
-    
+
     /* Find length of string */
     // while(*(s++)) len++;
 
-    // /* 
-    //  * Store each character from end of original string 
+    // /*
+    //  * Store each character from end of original string
     //  * to reverse string
     //  */
     // s--;
@@ -31,26 +31,24 @@ int main()
     //     len--;
     // }
     // *r = '\0';
-    reverse(str,rev);
+    reverse(str, rev);
     printf("\nOriginal string = %s\n", str);
     printf("Reverse string = %s", rev);
 
     return 0;
 }
 
-
-void reverse(char *str[],char *rev[]){
-    char *p=str,*q=rev;
-    int r=0;
-    while (*(p++)) r++;
+void reverse(char *str[], char *rev[])
+{
+    char *p = str, *q = rev;
+    int r = 0;
+    while (*(p++))
+        r++;
     p--;
-    while(r>=0)
+    while (r >= 0)
     {
-        *(q++)=*(--p);
+        *(q++) = *(--p);
         r--;
     }
-    *q='\0';
-      
-    
-
+    *q = '\0';
 }
