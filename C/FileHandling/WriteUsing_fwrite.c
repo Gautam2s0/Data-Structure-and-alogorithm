b3282cdf8380b366a85ab1beb2ada1224a82c0fb
Gautam2S0 <sonkarg117@gmail.com>
2023-11-28 14:50:09 +0530

    gg

---
+++ C/FileHandling/WriteUsing_fwrite.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct  bootk
+{
+   int bookId;
+   char title[100];
+   float price;
+
+}b1;
+
+
+void main(){
+    FILE *fp;
+    fp=fopen("f2.dat","wb");
+    printf("Enter the Book Id, Title and Price ");
+    scanf("%d",&b1.bookId);
+    fflush(stdin);
+    gets(b1.title);
+    
+    scanf("%f",&b1.price);
+    fwrite(&b1,sizeof(b1),1,fp);
+    fclose(fp);
+
+}
