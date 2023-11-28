86f3b73463944e1652f74745ea7159ef18cdc3ae
Gautam2S0 <sonkarg117@gmail.com>
2023-11-28 11:43:56 +0530

    27/11/23

--- C/FileHandling/Reading.c
+++ C/FileHandling/Reading.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-void main(){
+void main()
+{
     FILE *fp;
     char ch;
-    fp=fopen("f1.txt","r");
-    if(fp==NULL){
+    fp = fopen("f1.txt", "r");
+    if (fp == NULL)
+    {
         printf("File is not Exist");
     }
     // getting the character from file
-    ch=fgetc(fp);
-    while(!feof(fp)){ // feof check the end of file
-        printf("%c",ch);
-       ch=fgetc(fp);
+    ch = fgetc(fp);
+    while (!feof(fp)) // feof check the end of file
+    { 
+        printf("%c", ch);
+        ch = fgetc(fp);
     }
-    fclose(fp); // to close file 
-    
+    fclose(fp); // to close file
 }
