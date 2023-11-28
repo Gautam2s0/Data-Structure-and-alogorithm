7ae1142a9672f193fa8e8e3b2b3ea6210b39b78b
Gautam2S0 <sonkarg117@gmail.com>
2023-11-28 15:53:03 +0530

    gg

--- C/FileHandling/fscanf.c
+++ C/FileHandling/fscanf.c
@@ -5,6 +5,7 @@ void main(){
     int a,b,c;
     fp=fopen("f1.txt","r");
     //reading variable from file and storing them in above variable
+    // here , is data seperator delimeter
     fscanf(fp,"%d,%d,%d",&a,&b,&c);
     printf("a = %d b = %d c = %d",a,b,c);
     fclose(fp);
