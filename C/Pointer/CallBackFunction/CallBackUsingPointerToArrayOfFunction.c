62778e6b18610cbf22a3694beebea02ee7d7ffd9
Gautam2S0 <sonkarg117@gmail.com>
2023-11-29 13:34:11 +0530

    29/11/23

--- C/Pointer/CallBackFunction/CallBackUsingPointerToArrayOfFunction.c
+++ C/Pointer/CallBackFunction/CallBackUsingPointerToArrayOfFunction.c
@@ -9,27 +9,34 @@ void TimerExpire(void);
 void Defaut(void);
     
 
-// Array time of function pointer
+// Array of pointers to callback functions
 //declaring an array of function pointers named EventHandler, 
 //and each element of this array points to a function that takes no parameters (void parameters). 
 //The functions are ButtonClick, TimerExpire, SensorCatch, and Default.
 void (*EventHandler[])(void)={ButtonClick,TimerExpire,SensorCatch,Defaut};
 
 
+//Function that triggers events based on user input
 void Events(int event){
     if(event>=0 && event<3){
         (*EventHandler[event])();
+         return;
     }
+    (*EventHandler[3])();
+    
 }
 
 
 
 void main(){
     int i;
-    printf("\nEnter a Number for Event Handler : \n");
-    printf("\n0 for ButtonClick\n1 for TimerExpire\n2 for SensorCatch\n");
+    printf("\nEnter a Number for Event Handler\n");
+    printf("0 for ButtonClick\n1 for TimerExpire\n2 for SensorCatch\n: ");
     scanf("%d",&i);
-    Events(i);
+    // calling Event Function
+    // Events(i);
+    // without calling Event Function
+    (*EventHandler[i>=0?i<3?i:3:3])();
     
 
 }
