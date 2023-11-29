dc1954eadfc9488a699f581d662194492d79396a
Gautam2S0 <sonkarg117@gmail.com>
2023-11-29 10:47:04 +0530

    29/11/23

--- C/DynamicMemoeyAllocation/DMA1.c
+++ C/DynamicMemoeyAllocation/DMA1.c
@@ -1,16 +1,27 @@
-# include<stdio.h>
-# include<stdlib.h>
-  
+#include <stdio.h>
+#include <stdlib.h>
+
 void fun(int *a)
 {
-    a = (int*)malloc(sizeof(int));
+    a = (int *)malloc(sizeof(int));
 }
-  
+
 int main()
 {
     int *p;
     fun(p);
     *p = 6;
     printf("%d", *p);
-    return(0);
+    return (0);
 }
+
+// In the fun function, you are passing a pointer a by value. 
+//This means that changes made to a within the function will not affect the pointer in the main function.
+
+// Inside the fun function, you allocate memory for an integer using malloc, 
+//but the allocated memory is not being used or returned.
+
+// In the main function, you are dereferencing the uninitialized pointer p after calling fun. 
+//This will lead to undefined behavior.
+
+// Here is a corrected version of your code:
