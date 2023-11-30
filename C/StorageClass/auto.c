172e48857bd3d6c57181a3ec73261d93fe13d82c
Gautam2S0 <sonkarg117@gmail.com>
2023-11-30 17:14:59 +0530

    30/11/23

--- C/StorageClass/auto.c
+++ C/StorageClass/auto.c
@@ -2,9 +2,13 @@
 //Auto variables can be only accessed within the block/function they have been declared and not outside them (which defines their scope).
 // Of course, these can be accessed within nested blocks within the parent block/function in which the auto variable was declared.
 
+
 // auto storage class can't declare globally;
 // auto storage class can't declare globally;
 // however we have declare variable as global we don't use auto class;
+
+
+// scope of auto storage class is local/function/block
 
 #include <stdio.h>
 // can't declare as global;
