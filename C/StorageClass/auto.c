9773a0a62f1330cb3a6b718aa1a7f77a84e40f69
Gautam2S0 <sonkarg117@gmail.com>
2023-11-30 17:05:12 +0530

    30/11/23

---
+++ C/StorageClass/auto.c
@@ -0,0 +1,9 @@
+//This is the default storage class for all the variables declared inside a function or a block.
+//Auto variables can be only accessed within the block/function they have been declared and not outside them (which defines their scope).
+// Of course, these can be accessed within nested blocks within the parent block/function in which the auto variable was declared.
+
+#include <stdio.h>
+
+void main(){
+
+}
