b2580d178dcc8e680c428eb5eb8a4cef20a61238
Gautam2S0 <sonkarg117@gmail.com>
2023-11-30 17:23:08 +0530

    30/11/23

---
+++ C/StorageClass/Register.c
@@ -0,0 +1,14 @@
+// This storage class declares register variables that have the same functionality as that of the auto variables
+//The only difference is that the compiler tries to store these variables in the register of the microprocessor if a free register is available.
+
+//Use of register variables to be much faster than that of the variables stored in the memory during the runtime of the program.
+
+
+// If a free registration is not available, these are then stored in the memory only.
+
+// We are using register class where variable which are frequently use in progaram and improve the running time of program.
+
+// We cannot obtain the address of a register variable using pointers. 
+
+// Scope of register class variable is block/function
+
